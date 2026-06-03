138246e7f5f57bf992811ac4e100f48eca107d4a
cowscanfly <142424566+cowscanfly@users.noreply.github.com>
2026-06-03 13:20:42 +0700

    Change MemorySystem to a class

--- include/MemorySystem.hpp
+++ include/MemorySystem.hpp
@@ -4,24 +4,25 @@
 #include "types/PoolAllocator.hpp"
 
 namespace Engine {
-namespace Memory {
+class Memory {
 
-namespace {
-	inline Signal::Allocator* signalAllocator = nullptr;
-	inline PoolAllocator<PropertySignalNode> propertySignalNodeAllocator;
-}
+private:
+	inline static Signal::Allocator* signalAllocator = nullptr;
+	inline static PoolAllocator<PropertySignalNode> propertySignalNodeAllocator;
 
-inline void Initialize(Signal::Allocator* _signalAllocator) {
-	signalAllocator = _signalAllocator;
-}
+public:
+	static void Initialize(Signal::Allocator* _signalAllocator) {
+		signalAllocator = _signalAllocator;
+	}
 
-inline Signal::Allocator& GetSignalAllocator() {
-	return *signalAllocator;
-}
+	static Signal::Allocator& GetSignalAllocator() {
+		return *signalAllocator;
+	}
 
-inline PoolAllocator<PropertySignalNode>& GetPropertySignalNodeAllocator() {
-	return propertySignalNodeAllocator;
-}
+	static PoolAllocator<PropertySignalNode>& GetPropertySignalNodeAllocator() {
+		return propertySignalNodeAllocator;
+	}
 
-} // namespace Memory
+}; 
+
 } // namespace Engine
